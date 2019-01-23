685343be0e298e12c09aad51a63136d7d534e021
Yuansong Feng <yf57@duke.edu>
2019-01-23 16:03:17 -0500

    add routing to add ingredient page

--- hypomeals/urls.py
+++ hypomeals/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from .routers import router
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', include('inventorymanager.urls')),
     path('admin/', admin.site.urls),
     # rest api url routers
     path('api/', include(router.urls)),
+    # add ingredient page
+    path('add_ingredient', TemplateView.as_view(template_name='add_ingredient.html')),
 ]
