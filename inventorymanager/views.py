46a28df97d5d78aeae6cdd0d50f70dd09103c7f2
Naijiao <naijiaozhang489@gmail.com>
2019-01-24 22:05:48 -0500

    Created home page and links to features

--- inventorymanager/views.py
+++ inventorymanager/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
-	return HttpResponse("Hello. You are at starting point index.")
+	return render(request,'home.html')
 
 @login_required(login_url='/accounts/login/')
 def ingredient(request):
