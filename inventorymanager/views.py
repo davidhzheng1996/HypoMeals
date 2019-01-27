468b6e113f4f52dbb32a66b526446075dbd46dda
Yuansong Feng <yf57@duke.edu>
2019-01-27 01:25:09 -0500

    support bulk import gredient csv

--- inventorymanager/views.py
+++ inventorymanager/views.py
@@ -7,7 +7,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import *
+from .models import *
 
+import csv
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
@@ -33,10 +35,29 @@ class IngredientFileView(APIView):
 	# available parsers: https://www.django-rest-framework.org/api-guide/parsers/ 
 	# file sent should be in FormData
 	parser_classes = (MultiPartParser, FormParser)
+	# https://stackoverflow.com/questions/40663168/processing-an-uploaded-file-using-django
+	def process_file(self, csv_file):
+		with open(csv_file.name) as f:
+			reader = csv.DictReader(f)
+			for row in reader:
+				ingredient = Ingredient(ingredient_name=row['ingredient_name'], 
+									description=row['description'],
+									package_size=row['package_size'],
+									cpp=row['cpp'],
+									comment=row['comment'])
+				ingredient.save()
+	
 	def post(self, request, *args, **kwargs):
-		serializer = IngredientFileSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status.HTTP_201_CREATED)
+		# https://stackoverflow.com/questions/28545553/django-rest-frameworks-request-post-vs-request-data
+		# request.data is more flexible than request.FILES
+		file_serializer = IngredientFileSerializer(data=request.data)
+		if file_serializer.is_valid():
+			file_serializer.save()
+			# Use self.method() to access the function inside the same class...
+			# https://stackoverflow.com/questions/24813740/python-error-cannot-access-function-in-class 
+			self.process_file(request.data['file'])
+			return Response(file_serializer.data, status.HTTP_201_CREATED)
 		else:
-			return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+			return Response(file_serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+	
