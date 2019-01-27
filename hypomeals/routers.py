67c15eb75ef81543afb6c83c34d09ccbb313f204
Yuansong Feng <yf57@duke.edu>
2019-01-26 23:19:32 -0500

    set up ingredient file api endpoint

--- hypomeals/routers.py
+++ hypomeals/routers.py
@@ -1,10 +1,13 @@
 from rest_framework import routers
 from inventorymanager.viewsets import *
 
-# router maps rest api urls to correponding ViewSets 
+
+# https://www.django-rest-framework.org/api-guide/routers/
+# https://stackoverflow.com/questions/41379654/difference-between-apiview-class-and-viewsets-class/41380941
 router = routers.DefaultRouter()
 
-# map REST API url to corresponding ViewSet
+# map REST API url to corresponding ViewSet which provides a set of 
+# common api request logic handling 
 router.register(r'sku', SkuViewSet)
 router.register(r'customer', CustomerViewSet)
 router.register(r'ingredient', IngredientViewSet)
