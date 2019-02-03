d1786e591b4e83263435f5d762d12abcc101410e
Naijiao <naijiaozhang489@gmail.com>
2019-02-03 00:23:28 -0500

    Finished manufacture goals

--- inventorymanager/urls.py
+++ inventorymanager/urls.py
@@ -9,18 +9,21 @@ urlpatterns = [
     path('ingredient',views.ingredient),
     # map /sku to sku.html
     path('sku',views.sku),
-    path('manufacture_goal', views.manufacture_goal),
     path('product_line', views.product_line),
     path('goal',views.goal),
+    path('goal/<int:goalid>',views.manufacture_goal),
     # ingredient file upload endpoint
     # TODO how to integrate this with router's url patterns?
     path('api/ingredient_import/', IngredientImportView.as_view()),
     # ingredient file export endpoint 
     path('api/ingredient_export/', IngredientExportView.as_view()),
     path('api/manufacture_goal/', viewsets.manufacture_goals),
-    path('api/manufacture_goal/<int:id>', viewsets.manufacture_goals_get),
+    path('api/manufacture_goal/<int:id>/<int:goalid>', viewsets.manufacture_goals_get),
+    path('api/delete_manufacture_goal/<int:specificgoal>',viewsets.delete_manufacture_goal),
+    path('api/update_manufacture_goal/',viewsets.update_manufacture_goal),
     path('api/goal/<int:id>', viewsets.goal),
     path('api/delete_goal/<int:id>/<int:goalid>', viewsets.delete_goal),
+    path('api/update_goal/<int:id>/<int:goalid>', viewsets.update_goal),
     path('api/sku_import/', SkuImportView.as_view()),
     # ingredient file export endpoint 
     path('api/sku_export/', SkuExportView.as_view())
