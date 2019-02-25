6a0b389d969dd690bef7541cb1ccc429400f498a
Yuansong Feng <yf57@duke.edu>
2019-02-24 21:12:48 -0500

    format

--- static/js/sku.js
+++ static/js/sku.js
@@ -280,31 +280,6 @@ new Vue({
         })
       },
 
-      // selectFormulaCSV: function(event) {
-      //   this.formulaFile = event.target.files[0]
-      // },
-        
-      // uploadFormulaCSV: function() {
-      //   this.loading = true;
-      //   // upload this.ingredientCSV to REST api in FormData
-      //   const formData = new FormData()
-      //   // https://developer.mozilla.org/en-US/docs/Web/API/FormData/append
-      //   formData.append('file', this.formulaFile, this.formulaFile.name)
-      //   this.$http.post('/api/sku_formula_import/', formData)
-      //      .then((response) => {
-      //       this.upload_errors = response.data['errors'].join('\n') + response.data['warnings'].join('\n')
-      //       console.log(this.upload_errors)
-      //    this.loading = false;
-      //    this.csv_uploaded=true;
-      //    this.getSkus();
-      //    })
-      //      .catch((err) => {
-      //       this.upload_errors = err.data['errors'].join('\n') + err.data['warnings'].join('\n')
-      //    this.loading = false;
-      //    console.log(err);
-      //   })
-      // },
-
       exportSkuCSV: function() {
         this.loading = true;
         // Export all current skus to a csv file
@@ -320,71 +295,7 @@ new Vue({
         link.setAttribute("href", url);
         link.setAttribute("download", "sku.csv");
         link.click();
-        // this.loading = true;
-        // this.$http.get('/api/sku_export/')
-        //   .then((response) => {
-        //         // https://thewebtier.com/snippets/download-files-with-axios/
-        //         // https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
-        //         // url to the csv file in form of a Blob
-        //         // url lifetime is tied to the document in the window
-        //         const url = window.URL.createObjectURL(new Blob([response.data]));
-        //         // create a link with the file url and click on it
-        //         const link = document.createElement('a');
-        //         link.href = url;
-        //         link.setAttribute('download', 'sku.csv');
-        //         document.body.appendChild(link);
-        //         link.click();
-                
-        //         this.loading = false;
-        //         this.getSkus();
-        //   }).catch((err) => {
-        //         this.loading = false;
-        //         console.log(err)
-        //   })
       },
-
-      // exportFormula: function(){
-      //   this.loading = true;
-      //   // Export all current ingredients to a csv file
-      //   // https://codepen.io/dimaZubkov/pen/eKGdxN
-      //   let csvContent = "data:text/csv;charset=utf-8,";
-
-      //   csvContent+=[["Sku ID", "Ingr ID", "Quantity"].join(",")+'\n'];
-      //   let c = 0
-      //   //console.log(csvContent);
-      //   for (let i = 0; i < this.skus.length; i++) {
-      //           //console.log(this.ingredients[key].ingredient_name)
-      //           this.$http.get('/api/ingredients_to_sku/'+this.skus[i].id)
-      //          .then((response) => {
-      //             c = c + 1;
-      //              var ingredients = [];
-      //              this.ingredients = response.data;
-      //              for(let j = 0; j < this.ingredients.length; j++){
-      //                   csvContent+=[[this.skus[i].id,this.ingredients[j].id, this.ingredients[j].quantity].join(",")+'\n'];
-                      
-      //              }
-      //             // this.finished();
-      //             if(c==this.skus.length){
-      //                console.log(csvContent);
-                   
-      //                const url = encodeURI(csvContent);
-      //                 const link = document.createElement("a");
-      //                link.setAttribute("href", url);
-      //                link.setAttribute("download", "formulas.csv");
-      //                link.click();
-      //            }
-      //              this.loading = false;
-      //              //this.has_called = true; 
-      //          })
-      //          .catch((err) => {
-      //              this.loading = false;
-      //              console.log(err);
-      //          })
-
-      //   } 
-      
-
-      // },
 
         sortBy: function(key) {
         this.sortKey = key
