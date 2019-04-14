78c28a61ff62b8a85353d9145e868bd420cb6844
Naijiao <naijiaozhang489@gmail.com>
2019-04-14 19:45:03 -0400

    model for roles

--- inventorymanager/admin.py
+++ inventorymanager/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ('username','is_analyst','is_product_manager','is_business_manager','is_plant_manager','is_superuser')
+    list_filter = ('username', 'is_analyst','is_product_manager','is_business_manager','is_plant_manager','is_superuser')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Permissions', {'fields': ('is_analyst','is_product_manager','is_business_manager','is_plant_manager','is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'is_analyst','is_product_manager','is_business_manager','is_plant_manager','is_superuser')}
+        ),
+    )
+    search_fields = ('username',)
+    ordering = ('username',)
+
+
+admin.site.register(User, CustomUserAdmin)
